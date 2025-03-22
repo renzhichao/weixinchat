5b88e7c31bd73e183351ac61db11b0cad41346a6
Arthur.Ren <renzhichao@gmail.com>
2025-03-22 21:14:22 +0800

    download txt file from url to support wxcloudrun

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -20,12 +20,22 @@ def index(request, _):
     return render(request, 'index.html')
 """
 
-def trustdomain(request, _):
-    """
-    获取可信域名
-     `` request `` 请求对象 
-    """
-    return HttpResponse('LE4T696JBL6vLmly'.encode('utf-8'))
+import os
+
+def trustdomain(request):
+    # 定义 txt 文件的路径
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WW_verify_LE4T696JBL6vLmly.txt')
+    try:
+        # 打开文件以二进制模式读取
+        with open(file_path, 'rb') as file:
+            # 创建 HttpResponse 对象，并设置响应内容为文件内容
+            response = HttpResponse(file.read(), content_type='text/plain')
+            # 设置响应头，指定文件名，以便浏览器下载文件
+            response['Content-Disposition'] = 'attachment; filename=WW_verify_LE4T696JBL6vLmly.txt'
+            return response
+    except FileNotFoundError:
+        # 如果文件不存在，返回 404 错误
+        return HttpResponse(status=404)
 
 def counter(request, _):
     """
